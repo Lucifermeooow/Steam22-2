456c2984174419b5f42a6453fe34d8f71fc1d96f
Lucifermeooow <119759050+Lucifermeooow@users.noreply.github.com>
2026-08-31 05:42:55 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,24 @@
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
+
         maven {
             url = java.net.URI("https://jitpack.io")
+        }
+    }
+
+    configurations.configureEach {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "org.jetbrains.kotlin") {
+                useVersion("2.4.10")
+            }
         }
     }
 }
